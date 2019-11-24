53856dbddb737334e5b24f0cb3c92d30dee89220
Leonardo Motta <mail@leomotta.me>
2019-11-23 21:02:22 -0500

    add project base with typescript and eslint

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,18 +1,14 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
+import React, { ReactNode } from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import Header from './header'
+import './layout.css'
 
-import Header from "./header"
-import "./layout.css"
+interface Props {
+  children: ReactNode
+}
 
-const Layout = ({ children }) => {
+export default function Layout({ children }: Props) {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -44,9 +40,3 @@ const Layout = ({ children }) => {
     </>
   )
 }
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
-
-export default Layout
